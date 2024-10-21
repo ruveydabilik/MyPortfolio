b78fb758dccd05282b2fb5228ace8fed1d53ac39
Ayşe Rüveyda Bilik <121570981+ruveydabilik@users.noreply.github.com>
2024-10-22 00:28:19 +0300

    Migration is done and first controller is created

---
+++ MyPortfolio/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.DAL.Context
+{
+    public class MyPortfolioContext:DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public MyPortfolioContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // appsettings.json'daki bağlantı dizesini Dependency Injection (DI) ile alıyoruz
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contact { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
--- MyPortfolio/Program.cs
+++ MyPortfolio/Program.cs
@@ -1,7 +1,21 @@
+// EF Core'u veritabaný iþlemleri için kullanýyoruz.
+// EF Core Design ve tools paketleri code first iþlemleri için (migration) kullanýlacak olan paket
+// EF Core SQL Server, SQL'e iþlemleri yansýtabilmek için kullanýlacak
+
+using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DAL.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("config.json", optional: true, reloadOnChange: true);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Veritabaný baðlantýsýný config.json'dan al
+builder.Services.AddDbContext<MyPortfolioContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
