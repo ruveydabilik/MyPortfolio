bed080141d73bdb38303c16ea20afaf6f8e7c81e
Ayşe Rüveyda Bilik <121570981+ruveydabilik@users.noreply.github.com>
2024-10-23 01:30:17 +0300

    Partial components are created

---
+++ MyPortfolio/ViewComponents/ContactComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class ContactComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyPortfolio/ViewComponents/FeatureComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class FeatureComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
