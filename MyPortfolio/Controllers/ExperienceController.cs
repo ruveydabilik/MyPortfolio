e80248f864946643d97cc46204f63ddcff7206ae
Ayşe Rüveyda Bilik <121570981+ruveydabilik@users.noreply.github.com>
2024-10-26 18:00:33 +0300

    Experience operations are finished (on Admin Panel)

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -1,11 +1,80 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+using Newtonsoft.Json.Linq;
 
 namespace MyPortfolio.Controllers
 {
     public class ExperienceController : Controller
     {
-        public IActionResult ExperienceList()
+		private readonly MyPortfolioContext _portfolioContext;
+		public ExperienceController(MyPortfolioContext portfolioContext)
+		{
+			_portfolioContext = portfolioContext;
+		}
+		public IActionResult ExperienceList()
         {
+			var values = _portfolioContext.Experiences.ToList();
+			return View(values);
+		}
+
+		// Sayfa yüklenince çalışacak
+		[HttpGet]
+		public IActionResult CreateExperience() {
+            return View();
+        }
+
+		// Butonla tetiklenir
+		[HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            if (ModelState.IsValid)
+            {
+                _portfolioContext.Experiences.Add(experience); // Ekleme işlemini yap
+                _portfolioContext.SaveChanges(); // Veritabanına değişiklikleri kaydet
+                return RedirectToAction("ExperienceList"); // Tekrar yönlendirme yap
+            }
+            return View();
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = _portfolioContext.Experiences.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+            }else
+            {
+                _portfolioContext.Experiences.Remove(value);
+                _portfolioContext.SaveChanges();
+                return RedirectToAction("ExperienceList");
+            }
+        }
+
+        // GET: Düzenlenecek öğeyi göstermek için
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = _portfolioContext.Experiences.Find(id);
+            if(value == null)
+            {
+                return NotFound();
+            }
+            return View(value);
+        }
+
+        // POST: Güncelleme işlemini gerçekleştirmek için
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            if (ModelState.IsValid)
+            {
+                _portfolioContext.Experiences.Update(experience);
+                _portfolioContext.SaveChanges();
+                return RedirectToAction("ExperienceList");
+            }
             return View();
         }
     }
--- MyPortfolio/ViewComponents/FeatureComponentPartial.cs
+++ MyPortfolio/ViewComponents/FeatureComponentPartial.cs
@@ -6,7 +6,6 @@ namespace MyPortfolio.ViewComponents
     public class FeatureComponentPartial : ViewComponent
     {
         private readonly MyPortfolioContext _portfolioContext;
-
         public FeatureComponentPartial(MyPortfolioContext portfolioContext)
         {
             _portfolioContext = portfolioContext;
