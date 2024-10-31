c874f0fe9a6630a48d6088d592cc9bceeab66002
Ayşe Rüveyda Bilik <121570981+ruveydabilik@users.noreply.github.com>
2024-10-31 16:11:21 +0300

    About and feature operations are finished (on Admin Panel)

---
+++ MyPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly MyPortfolioContext _portfolioContext;
+        public AboutController(MyPortfolioContext portfolioContext)
+        {
+            _portfolioContext = portfolioContext;
+        }
+        public IActionResult About()
+        {
+            ViewBag.aboutId = _portfolioContext.Abouts.Select(x => x.AboutId).FirstOrDefault();
+            ViewBag.aboutTitle = _portfolioContext.Abouts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.aboutSubdescription = _portfolioContext.Abouts.Select(x => x.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetails = _portfolioContext.Abouts.Select(x => x.Details).FirstOrDefault();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult UpdateAbout(int id)
+        {
+            var value = _portfolioContext.Abouts.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            return View(value);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateAbout(About about)
+        {
+            if (ModelState.IsValid)
+            {
+                _portfolioContext.Abouts.Update(about);
+                _portfolioContext.SaveChanges();
+                return RedirectToAction("About");
+            }
+            return View();
+        }
+    }
+}
---
+++ MyPortfolio/Controllers/FeatureController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class FeatureController : Controller
+    {
+        private readonly MyPortfolioContext _portfolioContext;
+        public FeatureController(MyPortfolioContext portfolioContext)
+        {
+            _portfolioContext = portfolioContext;
+        }
+        public IActionResult Feature()
+        {
+            ViewBag.featureId = _portfolioContext.Features.Select(x => x.FeatureId).FirstOrDefault();
+            ViewBag.featureTitle = _portfolioContext.Features.Select(x => x.Title).FirstOrDefault();
+            ViewBag.featureDescription = _portfolioContext.Features.Select(x => x.Description).FirstOrDefault();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult UpdateFeature(int id)
+        {
+            var value = _portfolioContext.Features.Find(id);
+            if (value == null)
+            {
+                return NotFound();
+            }
+            return View(value);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult UpdateFeature(Feature feature)
+        {
+            if (ModelState.IsValid)
+            {
+                _portfolioContext.Features.Update(feature);
+                _portfolioContext.SaveChanges();
+                return RedirectToAction("Feature");
+            }
+            return View();
+        }
+    }
+}
